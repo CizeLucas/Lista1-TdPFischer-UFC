89d7e0c98d328a011ba069c6be1680ab468aea35
czluc <czluc@10.0.0.100>
2023-03-24 16:32:46 -0300

    Primeiro Commit

---
+++ Lista1/src/Questao01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Questao01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite A: ");
+		int A = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("Digite B: ");
+		int B = scanner.nextInt();
+		scanner.nextLine();
+	
+		
+
+		scanner.close();
+	}
+}
